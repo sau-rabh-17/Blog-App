b5dd7b18374d98778d74debeea03c75aba6117a1
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2025-08-12 23:57:41 +0530

    initial commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    accoutn;
+
+    constructor(){
+        this.client
+           .setEndpoint(conf.appwriteUrl)
+           .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount){
+                // call another method
+                return this.login({ email, password });
+            }else{
+                return userAccount;
+            }
+        }catch(err){
+            console.error("Error creating account:", err);
+            throw err;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            console.error("Error logging in:", error);
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.error("Error fetching current user:", error);
+            throw error;
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.error("Error logging out:", error);
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
